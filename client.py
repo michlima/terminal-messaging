88e54e2e5f317ef4b3f7d8a3c2219933f57307ae
Dimitri Kovzanadze <dimakovzanadze@gmail.com>
2025-12-04 15:36:37 +0100

    client

---
+++ client.py
@@ -0,0 +1,33 @@
+import random
+import socket
+import threading
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind("0.0.0.0", random.randint(8000, 9000))
+
+name = input ("Nickname: ")
+
+
+def recieve(): 
+    while True:
+        try:
+            message, _ = client.recvfrom(1024)
+            print(message.decode())
+        except: 
+            pass
+
+t = threading.Thread(target=recieve)
+t.start()
+
+client.sendto(f"SIGNUP_TAG:{name}".encode(), ("0.0.0.0", 9999))
+
+
+while True:
+    message = input("")
+    if message == "!q":
+        exit()
+    else:
+        client.sendto(f"{name}: {message}" .encode(), ("0.0.0.0", 9999))
+
+        
+    
